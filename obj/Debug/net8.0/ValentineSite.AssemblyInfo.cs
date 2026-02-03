fb9f7b3e2bdc665bfc1322da6ab909ff0ab5cf59
uomar <ST10315716@vcconnect.edu.za>
2026-02-03 21:53:35 +0200

    Initial commit - Valentine homepage project

--- obj/Debug/net8.0/ValentineSite.AssemblyInfo.cs
+++ obj/Debug/net8.0/ValentineSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ValentineSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42a2718b5ca2e9c46e284b2b767ed97a1b75713b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ValentineSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ValentineSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
